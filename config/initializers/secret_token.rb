987df8d60f35a5558c75b6559c62a65b387f4520
Ke Li <like3315416@live.ca>
2012-06-06 18:50:23 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Firstraildemo::Application.config.secret_token = '58b6e5e17fa7c90849b4ee09847762c297f8a43aeec4d5160cf32f81a7f1b33cdcebd57ae225e31ca04d39496a997cb0cecd5569b53164c43ef16c99025fca4d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Firstraildemo::Application.config.session_store :cookie_store, key: '_firstraildemo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Firstraildemo::Application.config.session_store :active_record_store
